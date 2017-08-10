f185b7d380844f474dda2c788782df2f83124253
Charlie Hildebrand <cdhildy@gmail.com>
2017-08-10 20:38:16 +0000

    Semicolon in contact_mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to 'cdhildy@gmail.com'
+    default to: 'cdhildy@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
